092fbd7a330c0c1a17ea8dd447e05d9d303c3a08
Luan Eto <luan.eto18@gmail.com>
2024-11-25 23:37:46 -0300

    Otimizar troca de temas botao moderador

--- componentes/sidebar/botaoSidebar/botaoPerfil/botaoPerfil.gd
+++ componentes/sidebar/botaoSidebar/botaoPerfil/botaoPerfil.gd
@@ -4,18 +4,25 @@ class_name BotaoPerfil
 @onready var _moderadorlabel = $"../ModeradorLabel"
 @onready var _separador4 = $"../Separador4"
 
+@onready var _temaNormal = preload("res://componentes/sidebar/botaoSidebar/botaoNormalSidebarTema.tres")
+@onready var _temaModerador = preload("res://componentes/sidebar/botaoSidebar/botaoPerfil/botaoModeradorSidebarTema.tres")
+
 func _ready():
 	connect("pressed", _on_pressed)
 	
 
-func _process(delta):
-	var temaBotao = preload("res://componentes/sidebar/botaoSidebar/botaoNormalSidebarTema.tres")
-	var isModerador = false
+func _process(_delta):
+	var temaBotao = _temaNormal
+	var isModerador = true if SessaoUsuario.isLogada and SessaoUsuario.usuarioLogado and SessaoUsuario.usuarioLogado.isModerador else false
 	
-	if SessaoUsuario.isLogada and SessaoUsuario.usuarioLogado and SessaoUsuario.usuarioLogado.isModerador:
-		temaBotao = preload("res://componentes/sidebar/botaoSidebar/botaoPerfil/botaoModeradorSidebarTema.tres")
-		isModerador = true
+	if isModerador:
+		temaBotao = _temaModerador
 		
-	_separador4.visible = !isModerador
-	_moderadorlabel.visible = isModerador
-	self.theme = temaBotao
+	if self.theme != temaBotao:
+		_separador4.visible = !isModerador
+		_moderadorlabel.visible = isModerador
+		trocarTema(temaBotao)
+
+
+func trocarTema(tema):
+	self.theme = tema
