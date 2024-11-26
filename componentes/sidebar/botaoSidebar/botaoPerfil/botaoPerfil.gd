1275b67020de9e46f3b3d38898fc315897d515b5
Luan Eto <luan.eto18@gmail.com>
2024-11-25 23:16:11 -0300

    Inserir tema para botao com moderador

--- componentes/sidebar/botaoSidebar/botaoPerfil/botaoPerfil.gd
+++ componentes/sidebar/botaoSidebar/botaoPerfil/botaoPerfil.gd
@@ -1,2 +1,21 @@
 extends BotaoMenuSidebar
 class_name BotaoPerfil
+
+@onready var _moderadorlabel = $"../ModeradorLabel"
+@onready var _separador4 = $"../Separador4"
+
+func _ready():
+	connect("pressed", _on_pressed)
+	
+
+func _process(delta):
+	var temaBotao = preload("res://componentes/sidebar/botaoSidebar/botaoNormalSidebarTema.tres")
+	var isModerador = false
+	
+	if SessaoUsuario.isLogada and SessaoUsuario.usuarioLogado and SessaoUsuario.usuarioLogado.isModerador:
+		temaBotao = preload("res://componentes/sidebar/botaoSidebar/botaoPerfil/botaoModeradorSidebarTema.tres")
+		isModerador = true
+		
+	_separador4.visible = !isModerador
+	_moderadorlabel.visible = isModerador
+	self.theme = temaBotao
